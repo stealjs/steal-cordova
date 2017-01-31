c7a3940c9374a78d3bd62f8e8cf5d76f6451a36e
Matthew Phillips <matthew@matthewphillips.info>
2017-01-31 08:29:21 -0500

    Sets up testing
    This adds a test, uses mocking to make sure that steal-cordova copies
    over the correct files.

---
+++ test/helpers.js
@@ -0,0 +1,31 @@
+var fse = require("fs-extra");
+
+exports.command = function(commands){
+	var cmd = commands.command;
+	return cmd && cmd[0];
+};
+
+exports.create = function(buildDir){
+	console.log('creating', buildDir)
+	return new Promise(function(resolve, reject){
+		fse.ensureDir(buildDir, function(err){
+			if(err) {
+				reject(err);
+			} else {
+				resolve();
+			}
+		})
+	});
+};
+
+exports.shimRunCli = function(stealCordova, cordovaOptions){
+	stealCordova.runCli = function(opts, commands){
+		switch(exports.command(commands)) {
+			case "create":
+				return exports.create(cordovaOptions.path);
+			case "build":
+				return Promise.resolve();
+				break;
+		}
+	};
+};
